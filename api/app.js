ee62a87594f9d25ee571bea40e2e5f3934016d6f
Arturo Sánchez <asanchezf@rotoplas.com>
2024-05-20 14:52:12 -0600

    Allowed Origins Whitelist
    Added a whitelist of allowed request origins

--- api/app.js
+++ api/app.js
@@ -4,13 +4,16 @@ import modelRoutes from "./routes/modelRoutes.js";
 import {handleError} from "./middleware/errorHandlerMiddleware.cjs";
 import {logRequest} from "./middleware/loggerMiddleware.cjs";
 import cors from "cors";
+
 export const app = express()
-const corsOptions = {
-    origin: '*',
-    methods: 'GET,PUT,POST',
-    allowedHeaders: ['Content-Type', 'application/json'],
-    credentials: false,
-    optionsSuccessStatus: 204
+let corsOptions = {
+    origin: (origin, callback) => {
+        console.log(origin);
+        if (!process.env.ALLOWED_ORIGINS.includes(origin))
+            callback(new Error(`Orígen ${origin} no permitido`), origin);
+        callback(null, origin);
+    },
+    optionsSuccessStatus: 200
 };
 app.use(logRequest);
 app.use(express.json());
